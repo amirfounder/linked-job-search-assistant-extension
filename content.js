030b66ec58384bfeb694fa952a06503aa5890605
Amir Sharapov <amir@amirfounder.com>
2022-06-08 19:32:22 -0500

    works perfectly

--- content.js
+++ content.js
@@ -16,22 +16,28 @@ const httpRequest = (method, route, body) => {
 }
 
 const sendRequest = (results) => {
+  let url_params
+  try {
+    url_params = Object.fromEntries(window.location.href.split('?')?.at(1)?.split('&')?.map(e => e?.split('=')))
+  } catch (err) {
+    url_params = {}
+  }
   const body = {
     timestamp: new Date().toISOString(),
     url: window.location.href,
-    url_params: Object.fromEntries(window.location.href.split('?')[1].split('&').map(e => e.split('='))),
+    url_params,
     results,
   }
   console.log(body)
-  httpRequest('POST', '/save-html', body)
+  httpRequest('POST', '/save-data', body)
 }
 
 const scrapePeopleSearchResults = () => {
   return Array.from(document.querySelectorAll('li.reusable-search__result-container'))
     .map(e => ({
-      link: e.querySelectorAll('a')[1].href,
-      name: e.querySelectorAll('a')[1].innerText.split('\n')[0],
-      position: e.querySelector('div.entity-result__primary-subtitle, t-14 t-black t-normal').innerText
+      link: e?.querySelectorAll('a')[1]?.href,
+      name: e?.querySelectorAll('a')[1]?.innerText?.split('\n')?.at(0),
+      position: e?.querySelector('div.entity-result__primary-subtitle, t-14 t-black t-normal')?.innerText
     }))
 }
 
@@ -41,10 +47,28 @@ if (window.location.href.includes('linkedin.com/search/results/people')) {
     sendRequest(results)
     setTimeout(() => {
       cleanup()
-    }, 5000)
+    }, 10000)
   }, 5000)
 }
 
+const scrapePersonProfile = () => ({
+  company: document.querySelector('a[href="#experience"]')?.innerText,
+  education: document.querySelector('a[href="#education"]')?.innerText,
+  profile_headlines: Array.from(
+    document
+    .querySelector('div.ph5, pb5')
+    .querySelector('.mt2, .relative')
+    .children[0]
+    .children
+  ).map(x => x?.innerText)
+})
+
 if (window.location.href.includes('linkedin.com/in/')) {
-  console.log('coming soon.')
+  setTimeout(() => {
+    const results = scrapePersonProfile()
+    sendRequest(results)
+    setTimeout(() => {
+      cleanup()
+    }, 10000)
+  }, 5000)
 }
